78d1e78d892f038471719503a81812cd0e238288
tripplicates <triplicatesouls@gmail.com>
2020-12-05 23:51:40 +0400

    [update] Update index.js, added 3 new functions

--- src/index.js
+++ src/index.js
@@ -1,22 +1,48 @@
-function control(){
+function getRowsCount(){
+    let count = document.querySelector('.header__counter').value;
+    return +count;
+
+}
+
+function btnError(count){
+    let btn = document.querySelector('.header__counter'),
+        result = false;
+    if(count > btn.max || count < btn.min){
+        btn.classList.add('header__counter_error');
+        setTimeout(() => {btn.classList.remove('header__counter_error')}, 500);
+        result = true;
+    }  
+
+    return result;
+}
+
+function camera(step){
+    let camera = document.querySelector('.camera');
+    
+    camera.scrollLeft = (camera.scrollWidth / 2) - 698;  
+    window.addEventListener('keydown', ({key}) => {
+        switch(key){
+            case "ArrowLeft" : camera.scrollLeft -= step; break;
+            case "ArrowRight" : camera.scrollLeft += step; break;
+        }
+    })
+}
+
+function createPyramid(){
     let btn = document.querySelector('.header__counter');
 
     btn.addEventListener('change', () => {
-        let rows = +btn.value;
-        if(rows > btn.max || rows < btn.min){
-            btn.classList.add('header__counter_error')
-            setTimeout(() => {btn.classList.remove('header__counter_error')}, 500);
-            return;
-        } else{
-            let instance = new Pyramid(rows);
-            instance.clearPyramid();
-            instance.createPyramid();
-        }
-       
+        let count = getRowsCount(),
+            isError = btnError(count),
+            instance = new Pyramid(count);
+
+        if(isError) return;
+        instance.clearPyramid();
+        instance.createPyramid();
+        camera(16) 
     })
     
-}; control();
-
+}; createPyramid();
 
 
 
