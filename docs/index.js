dd462c7197ec71fd8bf992fdaac08384bad5d258
tripplicates <triplicatesouls@gmail.com>
2020-12-06 00:14:53 +0400

    [update] Update index.js

--- docs/index.js
+++ docs/index.js
@@ -1,22 +1,55 @@
 "use strict";
 
-function control() {
-  var btn = document.querySelector('.header__counter');
-  btn.addEventListener('change', function () {
-    var rows = +btn.value;
+function getRowsCount() {
+  var count = document.querySelector('.header__counter').value;
+  return +count;
+}
 
-    if (rows > btn.max || rows < btn.min) {
-      btn.classList.add('header__counter_error');
-      setTimeout(function () {
-        btn.classList.remove('header__counter_error');
-      }, 500);
-      return;
-    } else {
-      var instance = new Pyramid(rows);
-      instance.clearPyramid();
-      instance.createPyramid();
+function btnError(count) {
+  var btn = document.querySelector('.header__counter'),
+      result = false;
+
+  if (count > btn.max || count < btn.min) {
+    btn.classList.add('header__counter_error');
+    setTimeout(function () {
+      btn.classList.remove('header__counter_error');
+    }, 500);
+    result = true;
+  }
+
+  return result;
+}
+
+function camera(step) {
+  var camera = document.querySelector('.camera');
+  camera.scrollLeft = camera.scrollWidth / 2 - 698;
+  window.addEventListener('keydown', function (_ref) {
+    var key = _ref.key;
+
+    switch (key) {
+      case "ArrowLeft":
+        camera.scrollLeft -= step;
+        break;
+
+      case "ArrowRight":
+        camera.scrollLeft += step;
+        break;
     }
   });
 }
 
-control();
+function createPyramid() {
+  var btn = document.querySelector('.header__counter');
+  btn.addEventListener('change', function () {
+    var count = getRowsCount(),
+        isError = btnError(count),
+        instance = new Pyramid(count);
+    if (isError) return;
+    instance.clearPyramid();
+    instance.createPyramid();
+    camera(16);
+  });
+}
+
+;
+createPyramid();
